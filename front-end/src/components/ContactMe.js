85cc48a50f44fdd8873b04655f63f3b00f66a635
gcampbell391 <gcampbell391@gmail.com>
2020-06-10 18:45:44 -0400

    Phase 6

--- front-end/src/components/ContactMe.js
+++ front-end/src/components/ContactMe.js
@@ -1,40 +1,9 @@
 import React from "react"
-import { Form, Button } from "semantic-ui-react"
 
 const ContactMe = (props) => {
     return (
         <div className="contactMe">
-            <h1 id="contactMeTitle">Contact Me</h1>
-            <Form size="large" onSubmit={props.handleSendClick}>
-                <Form.Input
-                    required
-                    fluid
-                    icon="user"
-                    iconPosition="left"
-                    placeholder="Name"
-                    name="inputName"
-                    id="inputName"
-                />
-                <Form.Input
-                    required
-                    fluid
-                    icon="envelope"
-                    iconPosition="left"
-                    placeholder="Email"
-                    name="inputEmail"
-                    id="inputEmail"
-                />
-                <Form.Input
-                    required
-                    fluid
-                    icon="keyboard"
-                    iconPosition="left"
-                    placeholder="Message"
-                    name="inputMessage"
-                    id="inputMessage"
-                />
-                <Button content='Send' icon='paper plane' color='teal' />
-            </Form>
+            <h1 id="contactMeTitle"> <a href="mailto:gcampbell391@gmail.com">Contact Me Today!</a></h1>
         </div>
     )
 }
