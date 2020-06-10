0dcc0f90e7e1e3735d413b0f33e5ed8fa9b308ae
gcampbell391 <gcampbell391@gmail.com>
2020-06-10 16:57:38 -0400

    Phase 4

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
     return (
         <div className="Footer">
-            <h1>Footer</h1>
+            <h3>Copyright © Gene Campbell III 2020</h3>
         </div>
     )
 }
